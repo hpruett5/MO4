b0a7f686ba1e0c6510f5406aff3d296a8c08ce12
Hannah Pruett <150202457+hpruett5@users.noreply.github.com>
2025-04-10 11:32:50 -0400

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,9 @@
+#zoo.py
+# 04-10-2025
+#Hannah Pruett
+# Part 1 
+#11.1 Task  Create a file called zoo.py. In it, define a function called hours() 
+# that prints the string 'Open 9-5 daily'.
+
+def hours():
+    print ("Open 9-5 daily")
